dc72ddc506e3750491c3da81ca1502b08db32398
Shreya Pai <ayerhs1997@gmail.com>
2021-01-23 22:45:59 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameBoardComponent } from './game-board/game-board.component';
+import { SquareComponent } from './square/square.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameBoardComponent,
+    SquareComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/game-board/game-board.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-board',
+  templateUrl: './game-board.component.html',
+  styleUrls: ['./game-board.component.css']
+})
+export class GameBoardComponent implements OnInit {
+  public board = [];
+  public boardSize = 9;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.createGameBoard();
+  }
+
+  public createGameBoard() {
+    for (let index = 1; index <= this.boardSize; index++) {
+      this.board.push( {
+        index: index,
+        state: null
+      })
+    }
+    return this.board;
+  }
+
+}
